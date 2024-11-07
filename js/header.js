699293f02c1b5b3eb0e50996b301ee2812aff015
blendy9702 <blendy9702@gmail.com>
2024-11-07 12:00:10 +0900

    [UI 자바스크립트] 슬라이드 예정

--- js/header.js
+++ js/header.js
@@ -29,4 +29,67 @@ window.addEventListener("load", function () {
 });
 
 // 이미지 바꾸기 및 메뉴 펼치기
-window.addEventListener("load", function () {});
+window.addEventListener("load", function () {
+  /* 
+  아이콘 이미지 바꾸기
+  <img src="경로" alt=""/>
+  위의 경로의 html 글자를 수정
+  클릭시 이미지 바꾸기 (토글)
+*/
+
+  // 1. 버튼 역할하는 ID 찾기
+  const mobileButton = document.querySelector("#mb-menu-bt");
+  // console.log(mobileButton);
+  // 2. 버튼 안에 있는 이미지를 찾기
+  const mobileButtonImage = document.querySelector("#mb-menu-bt img");
+  // console.log(mobileButtonImage);
+  // console.log(mobileButtonImage.attr("src"));
+
+  // 3. 버튼 안에 있는 이미지의 src는 무엇일까?
+  // <img src="./images/icon/icon-hbr.png" alt="모바일메뉴" />
+  // const srcString = mobileButtonImage.getAttribute("src");
+  // console.log(srcString);
+
+  // 4. 버튼 클릭 처리
+  const openIcon = "./images/icon/icon-hbr.png";
+  const closeIcon = "./images/icon/icon-close.png";
+
+  // 모바일 메뉴 관련 내용을 찾아서 이름을 주고 보관하자.
+  // 1. 모바일 배경
+  const mobileMenuBg = document.querySelector(".bg-mb-menu");
+  console.log(mobileMenuBg);
+  // 2. 모바일 메뉴
+  const mobileMenu = document.querySelector(".mb-menu");
+  console.log(mobileMenu);
+
+  mobileButton.addEventListener("click", function () {
+    // 이미지의 src 에 담겨진 글자를 수정하겠다
+    const imageSrc = mobileButtonImage.getAttribute("src");
+    // console.log(imageSrc);
+    if (imageSrc == openIcon) {
+      // 이미지 src 를 교체 하겠다.
+      mobileButtonImage.setAttribute("src", closeIcon);
+      mobileMenuBg.classList.add("bg-mb-menu-active");
+      mobileMenu.classList.add("mb-menu-active");
+    } else {
+      mobileButtonImage.setAttribute("src", openIcon);
+      mobileMenuBg.classList.remove("bg-mb-menu-active");
+      mobileMenu.classList.remove("mb-menu-active");
+    }
+  });
+
+  // 반응형 테스트
+  // pc 버전에서는 모두다 즉, 버튼도 초기화, 모바일 메뉴도 초기화
+  window.addEventListener("resize", function () {
+    // 웹브라우저의 넓이를 체크한다.
+    const windowWidth = window.innerWidth;
+    // console.log(windowWidth);
+    // css 코드에 반응형 작동이 1024px 부터 작동하니까.
+    if (windowWidth > 1024) {
+      //모바일 메뉴 버튼을 원래대로 되돌린다.
+      mobileButtonImage.setAttribute("src", openIcon);
+      mobileMenuBg.classList.remove("bg-mb-menu-active");
+      mobileMenu.classList.remove("mb-menu-active");
+    }
+  });
+});
